d0c834d97994e82964f0bd18904156d9873327d1
th3bossc <diljith2003@gmail.com>
2024-03-21 14:11:24 +0530

    minor changes

--- src/components/students/Card/index.tsx
+++ src/components/students/Card/index.tsx
@@ -1,6 +1,4 @@
 "use client";
-import Image from "next/image";
-import profile from "/public/userLogo.svg";
 import Button from "@/components/general/Button";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
@@ -23,8 +21,8 @@ const Card = ({
     const [show, setShow] = useState(false);
     const [current, setCurrent] = useState(0);
     const sampleImages = [
-        "/userLogo.svg",
-        "/userLogo.svg",
+        "/sampleProfile.jpeg",
+        "/sampleProfile.jpeg",
     ];
 
     useEffect(() => {
@@ -46,9 +44,12 @@ const Card = ({
                     >
                         <div className="flex w-full gap-4">
                             <motion.img
-                                src="/userLogo.svg"
+                                src="/sampleProfile.jpeg"
                                 alt="profile"
                                 layoutId={`student-card-${id}-image`}
+                                className="object-cover rounded-full"
+                                width={50}
+                                height={50}
                             />
                             <motion.div
                                 className="flex flex-col w-full"
@@ -92,14 +93,15 @@ const Card = ({
             {
                 show && (
                     <div
-                        className="w-screen h-screen bg-black/20 fixed top-0 left-0 z-[10000000] flex items-center justify-center p-8 md:p-16 lg:p-48"
+                        className="w-screen h-screen bg-black/20 fixed top-0 left-0 z-[10000000] flex items-center justify-center p-8 md:p-16 lg:p-48 backdrop-blur-sm"
+                        onClick={() => setShow(false)}
                     >
                         <motion.div
-                            className="w-full bg-white h-full lg:h-96 rounded-3xl font-inherit flex flex-col  lg:flex-row relative gap-8"
+                            className={`w-full bg-transparent h-full lg:h-96 rounded-3xl font-inherit flex flex-col overflow-hidden lg:flex-row relative gap-8`}
                             layoutId={`student-card-${id}`}
                         >
 
-                            <span className="absolute top-8 right-8 z-[10000000000]">
+                            <span className="hidden lg:inline bg-transparent absolute top-2 right-2 rounded-lg lg:top-8 lg:right-8 z-[10000000000]">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="h-8 w-8 text-black cursor-pointer"
@@ -116,64 +118,66 @@ const Card = ({
                                     />
                                 </svg>
                             </span>
-                            <div className="h-1/3 lg:h-full lg:w-1/3 rounded-3xl relative overflow-hidden outline">
-                                {/* <Image
+                            <div className={"w-full bg-white h-full lg:h-96 rounded-3xl font-inherit flex flex-col overflow-hidden lg:flex-row relative gap-8"}>
+                                <div className="h-1/3 lg:h-full lg:w-1/3 rounded-s-3xl relative overflow-hidden">
+                                    {/* <Image 
                                     className="w-full h-full object-cover"
                                     src={profile}
                                     alt="profile"
                                 /> */}
-                                <AnimatePresence>
-                                    <motion.img
-                                        className="w-full h-full object-cover"
-                                        src={sampleImages[current]}
-                                        layoutId={`student-card-${id}-image`}
-                                        alt="profile"
-                                        key={current}
-                                        initial={{ position: "absolute", x: "-100%" }}
-                                        animate={{ position: "relative", x: "0%" }}
-                                        exit={{ position: "absolute", x: "100%" }}
-                                    />
-                                </AnimatePresence>
-                            </div>
-                            <div className="w-full relative p-8">
-                                <motion.div
-                                    className="flex w-full flex-col"
-                                    layoutId={`student-card-${id}-header`}
-                                >
-                                    <span className="font-medium text-lg">{name}</span>
-                                    <span className="font-regular text-neutral-500">{course}</span>
-                                </motion.div>
-
-                                <hr className="text-white" />
-
-                                <motion.div
-                                    className="flex flex-col w-full mt-8 h-36"
-                                    layoutId={`student-card-${id}-content`}
-                                >
-                                    <span className="font-medium text-lg">Remarks</span>
-                                    <span className="font-regular text-neutral-500 h-20 overflow-hidden text-ellipsis">{remarks}</span>
-                                </motion.div>
-
-                                <motion.div
-                                    className="w-full mt-4 lg:mt-0 lg:absolute left-0 bottom-8 flex flex-col lg:flex-row lg:px-8 gap-4"
-                                    layoutId={`student-card-${id}-footer`}
-                                >
-                                    <div className="w-full lg:w-5/12 xl:w-1/3 text-xs">
-                                        <Button
-                                            text={contact}
-                                            rounded
-                                            invert
-                                            href={`tel:${contact}`}
+                                    <AnimatePresence>
+                                        <motion.img
+                                            className="w-full h-full object-cover"
+                                            src={sampleImages[current]}
+                                            layoutId={`student-card-${id}-image`}
+                                            alt="profile"
+                                            key={current}
+                                            initial={{ position: "absolute", x: "-100%" }}
+                                            animate={{ position: "relative", x: "0%" }}
+                                            exit={{ position: "absolute", x: "100%" }}
                                         />
-                                    </div>
-                                    <div className="w-full lg:w-7/12 xl:w-2/3 text-xs">
-                                        <Button
-                                            text={email}
-                                            rounded
-                                            href={`mailto:${email}`}
-                                        />
-                                    </div>
-                                </motion.div>
+                                    </AnimatePresence>
+                                </div>
+                                <div className="w-full relative p-8">
+                                    <motion.div
+                                        className="flex w-full flex-col"
+                                        layoutId={`student-card-${id}-header`}
+                                    >
+                                        <span className="font-medium text-lg">{name}</span>
+                                        <span className="font-regular text-neutral-500">{course}</span>
+                                    </motion.div>
+
+                                    <hr className="text-white" />
+
+                                    <motion.div
+                                        className="flex flex-col w-full mt-8 h-36"
+                                        layoutId={`student-card-${id}-content`}
+                                    >
+                                        <span className="font-medium text-lg">Remarks</span>
+                                        <span className="font-regular text-neutral-500 h-20 overflow-hidden text-ellipsis">{remarks}</span>
+                                    </motion.div>
+
+                                    <motion.div
+                                        className="w-full mt-4 lg:mt-0 lg:absolute left-0 bottom-8 flex flex-col lg:flex-row lg:px-8 gap-4"
+                                        layoutId={`student-card-${id}-footer`}
+                                    >
+                                        <div className="w-full lg:w-5/12 xl:w-1/3 text-xs">
+                                            <Button
+                                                text={contact}
+                                                rounded
+                                                invert
+                                                href={`tel:${contact}`}
+                                            />
+                                        </div>
+                                        <div className="w-full lg:w-7/12 xl:w-2/3 text-xs">
+                                            <Button
+                                                text={email}
+                                                rounded
+                                                href={`mailto:${email}`}
+                                            />
+                                        </div>
+                                    </motion.div>
+                                </div>
                             </div>
                         </motion.div>
                     </div>
