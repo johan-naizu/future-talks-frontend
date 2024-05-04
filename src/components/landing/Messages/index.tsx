4d4c0f142d7c475b9321f83fb50cc7288af6fc60
th3bossc <diljith2003@gmail.com>
2024-05-04 21:58:07 +0400

    adjusted mobile view

--- src/components/landing/Messages/index.tsx
+++ src/components/landing/Messages/index.tsx
@@ -10,9 +10,9 @@ import { useRouter } from "next/navigation";
 const Messages = () => {
     const router = useRouter();
     return (
-        <div className={`w-screen h-screen relative flex items-center justify-center bg-primarycolor p-20 font-bold text-md md:text-lg xl:text-4xl ${neueRegrade.className}`}>
-            <div className="w-full h-full flex flex-col items-center justify-center lg:grid grid-cols-7 grid-rows-2 gap-4">
-                <div className="col-span-4 rounded-[30px] bg-white p-[10px] w-4/5 lg:w-full h-96 lg:h-full">
+        <div className={`w-screen h-screen relative flex items-center justify-center bg-primarycolor p-20 font-bold text-md md:text-3xl xl:text-4xl ${neueRegrade.className}`}>
+            <div className="w-full h-full items-center justify-center grid grid-cols-4 lg:grid-cols-7 grid-rows-3 lg:grid-rows-2 gap-4">
+                <div className="col-span-4 rounded-[30px] bg-white p-[10px] w-full h-full">
                     <div className="rounded-[20px] bg-[#02566C] w-full h-full text-white flex items-center justify-center">
                         Not sure <br />
                         Which course to take? <br />
@@ -20,7 +20,7 @@ const Messages = () => {
                     </div>
                 </div>
                 <motion.button
-                    className="col-span-3 rounded-[30px] lg:rounded-br-none bg-white relative text-primarycolor flex items-center justify-center w-4/5 lg:w-full h-96 lg:h-full group"
+                    className="col-span-2 lg:col-span-3 rounded-[30px] lg:rounded-br-none bg-white relative text-primarycolor flex items-center justify-center w-full h-full group"
                     animate={{
                         scale: 1
                     }}
@@ -37,7 +37,7 @@ const Messages = () => {
                     <Image src={Arrow} alt="arrow" width={30} className="absolute bottom-4 right-4 group-hover:-rotate-180 transition" />
                 </motion.button>
                 <motion.button
-                    className="col-span-3 rounded-[30px] lg:rounded-tl-none bg-white relative text-primarycolor flex items-center justify-center w-4/5 lg:w-full h-96 lg:h-full group"
+                    className="col-span-2 lg:col-span-3 rounded-[30px] lg:rounded-tl-none bg-white relative text-primarycolor flex items-center justify-center w-full h-full group"
                     animate={{
                         scale: 1
                     }}
@@ -53,7 +53,7 @@ const Messages = () => {
                     With experts
                     <Image src={Arrow} alt="arrow" width={30} className="absolute bottom-4 right-4 group-hover:-rotate-180 transition" />
                 </motion.button>
-                <div className="col-span-4 rounded-[30px] bg-white p-[10px] w-4/5 lg:w-full h-96 lg:h-full">
+                <div className="col-span-4 rounded-[30px] bg-white p-[10px] w-full h-full">
                     <div className="rounded-[20px] bg-[#02566C] w-full h-full text-white flex items-center justify-center ">
                         Want to <br />
                         Get in touch with <br />
